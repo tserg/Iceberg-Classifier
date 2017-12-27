8ea5431674a228e54614a73b01a7bb3db0fb7ef6
Tse Rong Gary <tse.rong.gary@gmail.com>
2017-12-27 14:19:55 +0800

    feat: added band3 and normalisation
    Added band3 which is an average of band1 and band2. Values for band1, band2 and band3 are normalised. 
    
    Helper functions defined to build training and test sets.

--- iceberg.py
+++ iceberg.py
@@ -26,18 +26,58 @@ with open(training_data_path, 'r') as f:
     train_data = json.load(f)
     
 # Building training data
+
+def build_train_images(data):
+    """
+    Helper function to build images dataset from json
     
-images_train = []
-labels_train = []
-
-for image in train_data:
-    current_image_combined = np.reshape((image['band_1'] + image['band_2']), (75, 75, 2)) 
-    labels_train.append(image['is_iceberg'])
-    images_train.append(current_image_combined)
-
-images_train = np.array(images_train)
-labels_train = np.array(labels_train)
-
+    Parameters:
+        data
+        
+    Returns:
+        Array of images
+        Array of images labels
+    
+    """
+    
+    images = []
+    images_labels = []
+    
+    band1_data = [image['band_1'] for image in train_data]
+    band2_data = [image['band_2'] for image in train_data]
+    
+    # Normalize data : https://www.kaggle.com/vincento/keras-starter-4l-0-1694-lb-icebergchallenge
+    
+    band1_mean = np.mean(band1_data)
+    band1_max = np.max(band1_data)
+    band1_min = np.min(band1_data)
+    
+    band2_mean = np.mean(band2_data)
+    band2_max = np.max(band2_data)
+    band2_min = np.max(band1_data)
+    
+    for image in data:
+        current_image_band1 = np.reshape((image['band_1']), (75,75))
+        current_image_band1 = (current_image_band1-band1_mean) / (band1_max-band1_min)
+        
+        current_image_band2 = np.reshape((image['band_2']), (75,75))
+        current_image_band2 = (current_image_band2 - band2_mean) / (band2_max-band2_min)
+        
+        current_image_combined = np.reshape(([(band1 + band2)/2 for band1, band2 in zip(current_image_band1, current_image_band2)]), (75,75))
+        
+        # np.stack to give (75,75,3) shape
+        
+        current_image = np.stack((current_image_band1,
+                                  current_image_band2,
+                                  current_image_combined), axis=-1)
+    
+        images.append(current_image)
+        images_labels.append(image['is_iceberg'])
+        
+    
+    return np.array(images), np.array(images_labels)
+        
+images_train, labels_train = build_train_images(train_data)
 print (images_train[0].shape)
 
 # Building test data
@@ -48,16 +88,57 @@ test_data_path = "C:\\Users\\Gary\\iceberg-classifier\\data\\test\\test.json"
 with open(test_data_path, 'r') as f:
     test_data = json.load(f)
     
-images_test = []
-images_id_test = []
+def build_test_images(data):
+    """
+    Helper function to build images dataset from json
+    
+    Parameters:
+        data
+        
+    Returns:
+        Array of images
+        Array of images IDs
+    
+    """
+    
+    images = []
+    images_id = []
+    
+    band1_data = [image['band_1'] for image in test_data]
+    band2_data = [image['band_2'] for image in test_data]
+    
+    # Normalize data : https://www.kaggle.com/vincento/keras-starter-4l-0-1694-lb-icebergchallenge
+    
+    band1_mean = np.mean(band1_data)
+    band1_max = np.max(band1_data)
+    band1_min = np.min(band1_data)
+    
+    band2_mean = np.mean(band2_data)
+    band2_max = np.max(band2_data)
+    band2_min = np.max(band1_data)
+    
+    for image in data:
+  
+        current_image_band1 = np.reshape((image['band_1']), (75,75))
+        current_image_band1 = (current_image_band1-band1_mean) / (band1_max-band1_min)
+        
+        current_image_band2 = np.reshape((image['band_2']), (75,75))
+        current_image_band2 = (current_image_band2 - band2_mean) / (band2_max-band2_min)
+        
+        current_image_combined = np.reshape(([(band1 + band2)/2 for band1, band2 in zip(current_image_band1, current_image_band2)]), (75,75))
+        
+        # np.stack to give (75,75,3) shape
+        
+        current_image = np.stack((current_image_band1,
+                                  current_image_band2,
+                                  current_image_combined), axis=-1)
+        
+        images.append(current_image)
+        images_id.append(image['id'])
+    
+    return np.array(images), np.array(images_id)
 
-for image in test_data:
-    current_image_combined = np.reshape((image['band_1'] + image['band_2']), (75, 75, 2))
-    images_test.append(current_image_combined)
-    images_id_test.append(image['id'])
-
-images_test = np.array(images_test)
-
+images_test, images_id_test = build_test_images(test_data)
 print(images_test.shape)
 
 # Define the Model Architecture
@@ -65,7 +146,7 @@ print(images_test.shape)
 model = Sequential()
 
 model.add(Conv2D(filters = 16, kernel_size = 2, padding = 'same', activation='relu',
-                 input_shape=(75, 75, 2)))
+                 input_shape=(75, 75, 3)))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Dropout(0.2))
 
@@ -90,11 +171,15 @@ model.add(Flatten())
 model.add(Dense(500, activation = 'relu'))
 model.add(Dropout(0.3))
 
+'''
+# Additional dense layers does not improve score
+
 model.add(Dense(500, activation = 'relu')) # additional layer 1
 model.add(Dropout(0.3))
 
 model.add(Dense(500, activation = 'relu')) # additional layer 2
 model.add(Dropout(0.3))
+'''
 
 model.add(Dense(1, activation="sigmoid"))
 
@@ -107,7 +192,7 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 
 # Train the Model
 
-checkpointer = ModelCheckpoint(filepath='model_adam_2additional_dense.weights.best.hdf5', verbose=1, save_best_only=True)
+checkpointer = ModelCheckpoint(filepath='model_adam_test.weights.best.hdf5', verbose=1, save_best_only=True)
 
 hist = model.fit(images_train, labels_train, batch_size = 100, epochs = 100,
                  validation_split = 0.2,
@@ -115,7 +200,7 @@ hist = model.fit(images_train, labels_train, batch_size = 100, epochs = 100,
 
 # Load model
 
-model.load_weights('model_adam_2additional_dense.weights.best.hdf5')
+model.load_weights('model_adam_test.weights.best.hdf5')
 
 # Calculate accuracy on test set
 
@@ -127,7 +212,7 @@ print (predictions)
 print (len(predictions), len(images_id_test))
 
 
-with open('submission_adam_2additional_dense.csv', 'w', newline='') as f:
+with open('submission_adam_test.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(["id", "is_iceberg"])
     writer.writerows(zip(images_id_test, predictions))
